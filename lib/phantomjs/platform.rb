2842787b3507455ff55804436c18b324596d7db7
jeffborg <jeffrey@borgs.net>
2013-01-08 12:07:03 +1100

    Update lib/phantomjs/platform.rb
    Bump version to 1.8.1

--- lib/phantomjs/platform.rb
+++ lib/phantomjs/platform.rb
@@ -88,7 +88,7 @@ module Phantomjs
         end
 
         def package_url
-          'http://phantomjs.googlecode.com/files/phantomjs-1.7.0-linux-x86_64.tar.bz2'
+          'http://phantomjs.googlecode.com/files/phantomjs-1.8.1-linux-x86_64.tar.bz2'
         end
       end
     end
@@ -104,7 +104,7 @@ module Phantomjs
         end
 
         def package_url
-          'http://phantomjs.googlecode.com/files/phantomjs-1.7.0-linux-i686.tar.bz2'
+          'http://phantomjs.googlecode.com/files/phantomjs-1.8.1-linux-i686.tar.bz2'
         end
       end
     end
@@ -120,7 +120,7 @@ module Phantomjs
         end
 
         def package_url
-          'http://phantomjs.googlecode.com/files/phantomjs-1.7.0-macosx.zip'
+          'http://phantomjs.googlecode.com/files/phantomjs-1.8.1-macosx.zip'
         end
       end
     end
