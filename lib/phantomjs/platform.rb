accbfbdd2e82e00a8492c3fb0926b356d7ea08c3
Alexandre Leal <alexandre@omnilogic.com.br>
2013-09-24 12:50:18 -0300

    update phantom version from 1.9.0 to 1.9.2

--- lib/phantomjs/platform.rb
+++ lib/phantomjs/platform.rb
@@ -94,7 +94,7 @@ module Phantomjs
         end
 
         def package_url
-          'http://phantomjs.googlecode.com/files/phantomjs-1.9.0-linux-x86_64.tar.bz2'
+          'http://phantomjs.googlecode.com/files/phantomjs-1.9.2-linux-x86_64.tar.bz2'
         end
       end
     end
@@ -110,7 +110,7 @@ module Phantomjs
         end
 
         def package_url
-          'http://phantomjs.googlecode.com/files/phantomjs-1.9.0-linux-i686.tar.bz2'
+          'http://phantomjs.googlecode.com/files/phantomjs-1.9.2-linux-i686.tar.bz2'
         end
       end
     end
@@ -126,7 +126,7 @@ module Phantomjs
         end
 
         def package_url
-          'http://phantomjs.googlecode.com/files/phantomjs-1.9.0-macosx.zip'
+          'http://phantomjs.googlecode.com/files/phantomjs-1.9.2-macosx.zip'
         end
       end
     end
