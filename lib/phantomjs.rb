a50ed94f2b362d416a52e49e891ebdc27b666445
Alexander Groß <agross@therightstuff.de>
2015-12-09 14:21:59 +0100

    Fix missing parenthesis

--- lib/phantomjs.rb
+++ lib/phantomjs.rb
@@ -30,7 +30,7 @@ module Phantomjs
         platform.ensure_installed!
         platform
       else
-        raise UnknownPlatform, "Could not find an appropriate PhantomJS library for your platform (#{RUBY_PLATFORM} :( Please install manually."
+        raise UnknownPlatform, "Could not find an appropriate PhantomJS library for your platform (#{RUBY_PLATFORM}) :( Please install manually."
       end
     end
 
