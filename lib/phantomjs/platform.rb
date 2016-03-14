6f8629ba7015c95e1234ec7ba19256cf863dacd6
Alexander Groß <agross@therightstuff.de>
2016-03-14 20:09:48 +0100

    Work around Net::HTTP errors on Ruby 1.9

--- lib/phantomjs/platform.rb
+++ lib/phantomjs/platform.rb
@@ -109,7 +109,7 @@ module Phantomjs
                         uri.port,
                         use_ssl: uri.scheme == 'https',
                         verify_mode: OpenSSL::SSL::VERIFY_NONE) do |http|
-          request = Net::HTTP::Get.new(uri)
+          request = Net::HTTP::Get.new(uri.request_uri)
 
           http.request(request) do |response|
             case response
