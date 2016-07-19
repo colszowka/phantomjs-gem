de42ee2cab01d87a7df50242bcdf75260464ed5c
Karl He <khe@us.ibm.com>
2016-07-19 14:24:25 -0700

    Better windows support

--- lib/phantomjs/platform.rb
+++ lib/phantomjs/platform.rb
@@ -140,7 +140,7 @@ module Phantomjs
     class Win32 < Platform
       class << self
         def useable?
-          host_os =~ /mingw32|mswin|windows/ && architecture =~ /x86|i.86/
+          host_os =~ /mingw|mswin|cygwin/
         end
 
         def platform
@@ -148,7 +148,7 @@ module Phantomjs
         end
 
         def system_phantomjs_path
-          `where phantomjs`.delete("\n")
+          `where phantomjs`.split("\n")[0]
         rescue
         end
 
